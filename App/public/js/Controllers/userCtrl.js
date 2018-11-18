8ed730460a9826e988135e55028fdb20832172a1
LE Nhu Hien <nhuhien12895@gmail.com>
2018-11-18 02:36:00 +0100

    token returned saved as cookies

---
+++ App/public/js/Controllers/userCtrl.js
@@ -0,0 +1,29 @@
+// var loginPage = angular.module("loginPage", [])
+
+videoApp.controller('userCtrl', ['userService', '$scope', '$state','$cookies', function (userService, $scope, $state, $cookies) {
+    $scope.load = function () {
+
+    };
+
+
+
+    $scope.register = function () {
+       
+    };
+
+    $scope.login = function () {
+        userService.login($scope.email, $scope.password, function (res) {
+            if (res.data.success) {
+                username = res.data.username;
+                var token = res.data.token;
+                $cookies.put('token', token)
+                $state.go('home')
+                console.log($cookies.get('token'));
+                
+            } else
+                alert('email or password is not correct');
+        })
+    };
+
+    $scope.load();
+}])
--- App/serveur.js
+++ App/serveur.js
@@ -2,13 +2,13 @@ var path = require('path');
 
 
 const https = require("https"),
-fs = require("fs");
+  fs = require("fs");
 //helmet = require("helmet");
 
 
 const options = {
-  key: fs.readFileSync("D:/AMU/Semestre 9/Securite avancee/ResearchVideoSSI/SSL/research.com.key",'utf8'),
-  cert: fs.readFileSync("D:/AMU/Semestre 9/Securite avancee/ResearchVideoSSI/SSL/research.com.crt",'utf8'),
+  key: fs.readFileSync("D:/AMU/Semestre 9/Securite avancee/ResearchVideoSSI/SSL/research.com.key", 'utf8'),
+  cert: fs.readFileSync("D:/AMU/Semestre 9/Securite avancee/ResearchVideoSSI/SSL/research.com.crt", 'utf8'),
   //dhparam: fs.readFileSync("C:/Users/me/Desktop/Etudes/5A/SISecuProjet/SSL/dh-strong.pem",'utf8')
 };
 
@@ -16,20 +16,54 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var uuidv4 = require('uuid/v4');
 
-var loginLayer = require('./repository/loginLayer.js');
+var userRepository = require('./repository/userRepository.js');
 
 
 var app = express();
 
-//app.use(helmet()); // Add Helmet as a middleware
-
-
-
 var port = 8090;
+var jwt = require('jsonwebtoken');
+var Cookies = require("cookies");
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./config/private.pem');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
-https.createServer(options,app).listen(port,function() {
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/login', function (req, res) {
+  console.log(req.body.email)
+  var user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  userRepository.login(user, function (user, isFound) {
+    var token;
+    if (isFound) {
+      token = jwt.sign({
+        id: user._id,
+        email: user.email
+      }, RSA_PRIVATE_KEY, {
+        // algorithm: 'RS256',
+        expiresIn: 120
+      })
+      res.send({
+        success: isFound,
+        username: user.username,
+        token: token
+      });
+    } else
+      res.send({
+        success: isFound
+      })
+
+  })
+})
+
+
+https.createServer(options, app).listen(port, function () {
   console.log("Port : " + port);
 });
